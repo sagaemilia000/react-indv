b488fe9a852e4e71e0f47f71aedea9b0b48afb86
Saga Eriksson <sagaemilia000@gmail.com>
2025-04-30 11:50:42 +0200

    added ticketCounter in cart

--- src/components/cartItem/CartItem.jsx
+++ src/components/cartItem/CartItem.jsx
@@ -1,11 +1,23 @@
 import "./cartItem.css"
+import { useCartStore } from "../../store/cartStore";
 
 function CartItem({ item }) {
+
+    const { updateQuantity } = useCartStore();
+
+    const increase = () => updateQuantity(item.id, item.quantity + 1);
+    const decrease = () => updateQuantity(item.id, item.quantity - 1);
+
     return (
-        <li>
+        <li className="cart-item">
             <h2>{item.name}</h2>
             <p>{item.date} kl {item.from} - {item.to}</p>
             <p>{item.quantity}</p>
+            <div className="cart-item__controls">
+                <button onClick={decrease}>–</button>
+                <span>{item.quantity}</span>
+                <button onClick={increase}>+</button>
+            </div>
         </li>
     )
 }
