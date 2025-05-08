4c2e369e176e0b1b063058306f9ad94ada54bf7a
Saga Eriksson <sagaemilia000@gmail.com>
2025-05-08 11:52:17 +0200

    added add to cart message with react toastify and moved confetti to tickets

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,41 @@ import EventDetailsPage from "./pages/eventDetailsPage/EventDetailsPage";
 import CartPage from "./pages/cartPage/CartPage";
 import TicketPage from "./pages/ticketPage/TicketPage";
 import ErrorPage from "./pages/errorPage/ErrorPage";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/"
+            element= {<LandingPage/>} />
+          <Route 
+            path="/events"
+            element= {<EventsPage/>} />
         <Route 
-          path="/"
-          element= {<LandingPage/>} />
-        <Route 
-          path="/events"
-          element= {<EventsPage/>} />
-      <Route 
-          path="/details/:id"
-          element= {<EventDetailsPage/>} />
-        <Route 
-          path="/cart"
-          element= {<CartPage/>} />
-        <Route 
-          path="/tickets"
-          element= {<TicketPage/>} />
-        <Route 
-          path="*"
-          element= {<ErrorPage/>} />
-      </Routes>
-    </BrowserRouter>
-    
+            path="/details/:id"
+            element= {<EventDetailsPage/>} />
+          <Route 
+            path="/cart"
+            element= {<CartPage/>} />
+          <Route 
+            path="/tickets"
+            element= {<TicketPage/>} />
+          <Route 
+            path="*"
+            element= {<ErrorPage/>} />
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer 
+        position="top-center" 
+        autoClose={3000}
+        toastClassName="custom-toast"
+        bodyClassName="custom-toast-body" />
+    </>
   )
 }
 
